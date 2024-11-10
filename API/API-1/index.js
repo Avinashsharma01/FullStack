283921f0b0a0236290a6dfef1a240b059e2ca373
Avinash Sharma <Avinashsharma31384@gmail.com>
2024-11-10 19:34:56 +0530

    Api-2 added

--- API/API-1/index.js
+++ API/API-1/index.js
@@ -1,31 +1,27 @@
-import express from 'express';
-import router from './routes/myRoute.js';
-import path from 'path';
-import dotenv from 'dotenv';
-dotenv.config();
-import connectDB from './Database/database.js';
-import cors from 'cors';
-import the_product from './models/the_product.js';
+import express from 'express'; // Importing the express module
+import router from './routes/myRoute.js'; // Importing the router from the specified file
+import path from 'path'; // Importing the path module
+import dotenv from 'dotenv'; // Importing the dotenv module to load environment variables
+dotenv.config(); // Loading environment variables from a .env file
+import connectDB from './Database/database.js'; // Importing the database connection function
+import cors from 'cors'; // Importing the CORS middleware
+import the_product from './models/the_product.js'; // Importing the product model
 
+const app = express(); // Creating an instance of express
+const port = process.env.PORT || 34545; // Setting the port from environment variables or default to 34545
 
-const app = express();
-const port = process.env.PORT || 34545;
+app.use(cors()); // Enabling CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use(express.static(path.join(path.resolve(), "public"))); // Serving static files from the 'public' directory
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(path.resolve(), "public")));
-
-
-
-// routes start frome here
-app.use("/api", router);
-
+// Routes start from here
+app.use("/api", router); // Using the router for all routes starting with /api
 
 const server = async () => {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL); // Connecting to the database
     app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+        console.log(`Server is running on port ${port}`); // Starting the server and logging a message
     });
 }
-server();
+server(); // Calling the server function to start the server
